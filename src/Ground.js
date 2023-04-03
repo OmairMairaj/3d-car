e1e2ee1f7e53f2c417803bfd8336bd06384dd2e6
unknown <omairmairaj@gmail.com>
2023-04-02 22:03:57 -0700

    OMA

--- src/Ground.js
+++ src/Ground.js
@@ -1,5 +1,5 @@
 import { MeshReflectorMaterial } from "@react-three/drei";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { useEffect } from "react";
 import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
 
@@ -20,6 +20,12 @@ export function Ground(){
 
         normal.encoding = LinearEncoding;
     } , [normal, roughness]);
+
+    useFrame((state, delta) => {
+        let t = -state.clock.getElapsedTime() * 0.128;
+        roughness.offset.set(0, t);
+        normal.offset.set(0, t);
+    })
 
 
     return(
